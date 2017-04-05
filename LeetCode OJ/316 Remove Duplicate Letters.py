c291bba8fa6f61a09a6df612cdf81cd0396b8371
Culiner <rz.chen@foxmail.com>
2017-04-05 21:15:30 +0800

    add question No.316

---
+++ LeetCode OJ/316 Remove Duplicate Letters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        cnt = [0]*26 # Counters needed
+        res = '' # And an answer container
+        used = [False]*26 # Some used marks as well 
+        for c in s:
+            cnt[ord(c) - ord('a')] += 1
+        for c in s:
+            ci = ord(c) - ord('a')
+            cnt[ci] -= 1
+            if used[ci]:    continue
+            for j in xrange(len(res)-1, -1, -1): # Checking backward for some dulplicate leters
+                cj = ord(res[j]) - ord('a')
+                if cj > ci and cnt[cj] > 0:
+                    used[cj] = False
+                    res = res[:j] + res[j+1:]
+                    j -= 1
+                else:    break
+            used[ci] = True
+            res += c
+        return res
+
+print Solution().removeDuplicateLetters("cbacdcbc")
